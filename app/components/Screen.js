457edad33836c737e8b3064416924ac5663ac2cf
Victor <victormarques.ia@gmail.com>
2020-07-31 15:56:59 -0300

    Create Screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+const Screen = ({ children }) => {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+});
+
+export default Screen;
