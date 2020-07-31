d910992b9dcb5c316adac32d1d5530d2cf26fbbe
Victor <victormarques.ia@gmail.com>
2020-07-30 23:45:56 -0300

    Create ListDetailScreen

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import colors from '../config/colors';
+
+import ListItem from '../components/ListItem';
+
+const ListingDetailsScreen = () => {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/mosh.jpg')}
+            title="Mosh Homedani"
+            subTitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 40,
+  },
+});
+
+export default ListingDetailsScreen;
