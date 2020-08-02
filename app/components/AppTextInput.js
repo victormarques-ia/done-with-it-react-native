f311d8ec000d43f13b8a24f1e3a6c85a6b3f0fb0
Victor <victormarques.ia@gmail.com>
2020-08-02 08:19:00 -0300

    Update input items for dynamic width

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, TextInput, Platform } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import defaultStyles from '../config/styles';
 
-const AppTextInput = ({ icon, ...otherProps }) => {
+const AppTextInput = ({ icon, width = '100%', ...otherProps }) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { width }]}>
       {icon && (
         <MaterialCommunityIcons
           name={icon}
@@ -28,7 +28,6 @@ const styles = StyleSheet.create({
     backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: 'row',
-    width: '100%',
     padding: 15,
     marginVertical: 10,
   },
