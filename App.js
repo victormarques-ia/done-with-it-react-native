45c7b7b683217601cabd48682a67226f6a46504a
Victor Silva <victormarques.ia@gmail.com>
2020-08-15 11:36:55 -0300

    Update App file

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import LoginScreen from './app/screens/LoginScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
+
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingEditScreen />;
 }
 
-const styles = StyleSheet.create({});
