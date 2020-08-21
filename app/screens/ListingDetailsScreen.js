9c67f7452fda3490fc5946553dc4d58dcbd99005
Victor Silva <victormarques.ia@gmail.com>
2020-08-21 16:20:29 -0300

    Update ListingDetailsScreen

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,22 +1,28 @@
-import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Image } from 'react-native-expo-image-cache';
 
-import colors from "../config/colors";
-import ListItem from "../components/lists/ListItem";
-import Text from "../components/Text";
+import colors from '../config/colors';
+import ListItem from '../components/lists/ListItem';
+import Text from '../components/Text';
 
 function ListingDetailsScreen({ route }) {
   const listing = route.params;
 
   return (
     <View>
-      <Image style={styles.image} source={listing.image} />
+      <Image
+        style={styles.image}
+        tint="light"
+        preview={{ uri: listing.images[0].thumbnailUrl }}
+        uri={listing.images[0].url}
+      />
       <View style={styles.detailsContainer}>
         <Text style={styles.title}>{listing.title}</Text>
         <Text style={styles.price}>${listing.price}</Text>
         <View style={styles.userContainer}>
           <ListItem
-            image={require("../assets/mosh.jpg")}
+            image={require('../assets/mosh.jpg')}
             title="Mosh Hamedani"
             subTitle="5 Listings"
           />
@@ -31,18 +37,18 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   image: {
-    width: "100%",
+    width: '100%',
     height: 300,
   },
   price: {
     color: colors.secondary,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 20,
     marginVertical: 10,
   },
   title: {
     fontSize: 24,
-    fontWeight: "500",
+    fontWeight: '500',
   },
   userContainer: {
     marginVertical: 40,
