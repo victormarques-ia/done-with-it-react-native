f1586fe8ff1ce10947b2a9254849a1765eaacef3
Victor Silva <victormarques.ia@gmail.com>
2020-08-23 07:47:51 -0300

    Update LoginScreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,25 +1,52 @@
-import React from "react";
-import { StyleSheet, Image } from "react-native";
-import * as Yup from "yup";
+import React, { useState, useContext } from 'react';
+import { StyleSheet, Image } from 'react-native';
+import * as Yup from 'yup';
 
-import Screen from "../components/Screen";
-import { Form, FormField, SubmitButton } from "../components/forms";
+import Screen from '../components/Screen';
+import {
+  ErrorMessage,
+  Form,
+  FormField,
+  SubmitButton,
+} from '../components/forms';
+import authApi from '../api/auth';
+import AuthContext from '../auth/context';
+
+import jwtDecode from 'jwt-decode';
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().min(4).label("Password"),
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password'),
 });
 
 function LoginScreen(props) {
+  const authContext = useContext(AuthContext);
+  const [loginFailed, setLoginFailed] = useState(false);
+
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password);
+
+    if (!result.ok) {
+      setLoginFailed(true);
+      return;
+    }
+    setLoginFailed(false);
+    const user = jwtDecode(result.data);
+    authContext.setUser(user);
+  };
+
   return (
     <Screen style={styles.container}>
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
 
       <Form
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
-        validationSchema={validationSchema}
-      >
+        initialValues={{ email: '', password: '' }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}>
+        <ErrorMessage
+          error="Invalid email and/or password."
+          visible={loginFailed}
+        />
         <FormField
           autoCapitalize="none"
           autoCorrect={false}
@@ -51,7 +78,7 @@ const styles = StyleSheet.create({
   logo: {
     width: 80,
     height: 80,
-    alignSelf: "center",
+    alignSelf: 'center',
     marginTop: 50,
     marginBottom: 20,
   },
