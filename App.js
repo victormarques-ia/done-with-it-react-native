73ce22fb798c2fbbac68860688c8866d5be182cc
Victor Silva <victormarques.ia@gmail.com>
2020-08-23 07:48:07 -0300

    Update App

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthContext from './app/auth/context';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
+  const [user, setUser] = useState();
+
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
